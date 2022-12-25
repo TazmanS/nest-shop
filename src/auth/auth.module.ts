a0a4189add156960a2a1f777d95e4c618fe012e5
evgeniy.sh1 <evgeniy.sh@filancy.com>
2022-12-25 14:52:49 +0200

    jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,25 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('NEST_JWT_TOKEN_SECRET'),
+          signOptions: { expiresIn: '60s' }
+        }
+      },
+      inject: [ConfigService]
+    })
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService]
 })
 export class AuthModule { }
